75d820b133ec1ee3c7e9f95fdbfca308f7dcea85
Stacey <staceyelsom@gmail.com>
2022-02-14 15:23:05 +0000

    Updated test, extended requires, added seperate test for status and return

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,35 @@
-const { request } = require("supertest");
-const app = require("../app.js")
-  
+const request = require("supertest");
+const app = require("../app.js");
+const seed = require('../db/seeds/seed')
+const data = require('../db/data/test-data')
+const db = require('../db/connection.js')
+
+beforeEach(() => seed(data))
+
+afterAll(() => {
+  if (db.end) db.end();
+});
+
   describe("GET /api/topics", () => {
-    test("Status 200, returns an array of objects with properties 'slug', 'description", () => {
+    test("Status 200", () => {
       return request(app)
         .get("/api/topics")
         .expect(200)
+    });
+    test("returns an array of objects with properties 'slug', 'description", () => {
+        return request(app)
+            .get("/api/topics")
+            .expect(200)
+        .then((response) => {
+            console.log(response.body)
+            expect(response.body).toHaveLength(3);
+            response.body.forEach((cell) => {
+                expect(cell).toEqual(
+                  {
+                    description: expect.any(String),
+                    slug: expect.any(String)
+                  })
+              });
+        })
     })
 })
