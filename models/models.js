1780bfb3da51410878d35596f084d0cceccad1fa
Stacey <staceyelsom@gmail.com>
2022-02-15 15:20:51 +0000

    Refactored fetchArticle to use WHERE in SQL and check data of [0] in returned information

--- models/models.js
+++ models/models.js
@@ -1,24 +1,25 @@
 const db = require('../db/connection.js')
 
 function fetchTopics() {
-    return db.query("SELECT * FROM topics")
+    return db.query("SELECT * FROM topics;")
         .then((data) => {
             return data.rows
         })
 }
 
 function fetchArticles() {
-    return db.query("SELECT * FROM articles")
+    return db.query("SELECT * FROM articles;")
         .then((data) => {
             return data.rows
         })
 }
 
 function fetchArticle(num) {
-    return db.query("SELECT * FROM articles")
+    return db.query(`SELECT * FROM articles
+                    WHERE article_id = ${num};`)
         .then((data) => {
-            if (!data.rows[num]) return Promise.reject();
-            return data.rows[num];
+            if (!data.rows[0]) return Promise.reject();
+            return data.rows[0];
         })
 }
 
